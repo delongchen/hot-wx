59679326a7956ba934952f1a6965b16ce2560186
a2670392079 <37444340+a2670392079@users.noreply.github.com>
2021-10-15 18:17:10 +0800

    feat: nothing

---
+++ src/views/porn/components/PornCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="video-card-reco">
+
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "PornCard"
+}
+</script>
+
+<style scoped lang="less">
+.video-card-reco {
+  position: relative;
+  width: 206px;
+  height: 116px
+}
+</style>
